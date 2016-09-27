8387ae4bf107eac5cef8e91d62b463ee5a77b2b8
Dave Voyles <Startingline13@hotmail.com>
2016-09-27 10:20:10 -0400

    cleaning up more code

--- Node-Server/public/newClient.js
+++ Node-Server/public/newClient.js
@@ -5,19 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
   var width   = window.innerWidth;
   var height  = window.innerHeight;
 
-
-// TODO:
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
-
-////////////////////////////////////////////////////
+    byId('sendAsDiv').onclick    = SendAsDiv;
+    byId('sendAsBinary').onclick = SendAsBinary;
 
 
 // Get the image
 var sampleImage = document.getElementById("ringoImage"),
     canvas = convertImageToCanvas(sampleImage),
-    image = convertCanvasToImage(canvas),
-    canvas2 = convertImageToCanvas(image),
-    image2 = convertCanvasToImage(canvas2);
+    image = convertCanvasToImage(canvas);
     var context = canvas.getContext('2d'); 
 
 
@@ -26,35 +21,6 @@ var sampleImage = document.getElementById("ringoImage"),
     document.getElementById("pngHolder").appendChild(image);
     
 
-    /**
-     * After receiving image from server, create a new canvas out of it.
-     * @param {Image} image - Image to draw on canvas.
-     * @return {canvas} Canvas with image drawn on it.
-     */
-    function convertImageToCanvas(image) {
-    var canvas        = document.createElement("canvas");
-        canvas.width  = image.width;
-        canvas.height = image.height;
-        canvas.getContext("2d").drawImage(image, 0, 0);
-        
-        return canvas;
-    }
-    
-
-    /**
-     * Creates a PNG image from canvas w/ canvas.toDataURL()
-     * @return {Image} Image from canvas
-     */
-    function convertCanvasToImage(canvas) {
-        var image     = new Image();
-        log('canvas is:');
-        log(canvas);
-        image.src = canvas.toDataURL("image/png");
-
-        return image;
-    }
-
-    ////////////////////////////////////////////////////////
 
 
    function drawLines (data) {
@@ -67,48 +33,6 @@ var sampleImage = document.getElementById("ringoImage"),
       context.strokeStyle = data.color;
       context.stroke();
    }
-
-
-   /**
-    * Called by SendImageToBytes. Creates an image based on convertCanvasToImage.
-    * Canvas -> Image -> Bytes
-    * @return {Uint8Array} Bytes from the newly converted canvas.
-    */
-    function ConvertImgToBytes() {
-        var image  = convertCanvasToImage(canvas);
-        var buffer = new ArrayBuffer(image.data.length);
-        var bytes  = new Uint8Array(buffer);
-
-        for (var i=0; i<bytes.length; i++) {
-            bytes[i] = image.data[i];
-        }
-        debug.log("ConvertImgToBytes:" );
-        Console.log(bytes);
-
-        return bytes;
-    }
-
-
-    /**
-     * Accepts bytes from server & converts to an image.
-     * @return {ImageData} Image data from byte array
-     */
-    function ConvertBytesToImg (blob) {
-        var bytes     = new Uint8Array(blob);        
-        var imageData = context.createImageData(canvas.width, canvas.height);
-        
-        // Copy bytes to new imageData obj
-        for (var i=8; i<imageData.data.length; i++) {
-            imageData.data[i] = bytes[i];
-        }
-        // This can paint the canvas with the new-found image
-        // context.putImageData(imageData, 0, 0);
-
-        // OR
-
-        // Returns new image    
-        return imageData;
-    }  
 
 
    var mouse = { 
@@ -176,7 +100,10 @@ var sampleImage = document.getElementById("ringoImage"),
    /////////////////////////////////////////////////////////
 
 
-    byId('sendAsDiv').onclick = SendAsDiv;
+    /**
+     * Convert canvas to data URI containing a representation of the image. Defaults to PNG, 96dpi resolution.
+     * Create a new element, set URI as the source, & pass URI to node server.
+     */
     function SendAsDiv() {
         var encodedCanvas    = canvas.toDataURL();
         var imageElement     = new Image(250, 250);
@@ -196,8 +123,11 @@ var sampleImage = document.getElementById("ringoImage"),
 
 
 
-    // OUTPUT: data:image/png;base64,iVBORw0K......
-    byId('sendAsBinary').onclick = SendAsBinary;
+    /**
+     * Convert canvas to data URI containing a representation of the image. Defaults to PNG, 96dpi resolution.
+     * Passes URI to node server.
+     * OUTPUT: data:image/png;base64,iVBORw0K......
+     */ 
     function SendAsBinary() {
         var encodedCanvas    = canvas.toDataURL();
         socket.emit('imgBinary', payload);
@@ -212,36 +142,76 @@ var sampleImage = document.getElementById("ringoImage"),
   });
 
 
+    // draw line received from server
+	socket.on('draw_line', function (data) {
+        drawLines(data);
+   });
 
-
-
-
-   // Convert canvas to image, and tell server to convert those bytes to an image
-   byId('send').onclick = SendImageToBytes;  
-   function SendImageToBytes() {    
-         var bytes = ConvertImgToBytes(image);
-        log('sending image binary. bytes: ');
-        log(bytes);
-
-        socket.emit('ConvertedImgToBytes', bytes);
+   
+   /**
+    * Main loop, running every 25ms
+    * Emits line data to node server, so that other clients can replicate lines
+    */
+   function mainLoop() {
+      // check if the user is drawing
+      if (mouse.click && mouse.move && mouse.pos_prev) {
+         // send line to to the server
+         socket.emit('draw_line', {
+              line: [ mouse.pos, mouse.pos_prev ] ,
+              color: context.curColor // BUG: not working yet
+            });
+         mouse.move = false;
+      }
+      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+      setTimeout(mainLoop, 25);
    }
+   mainLoop();
 
 
-   // Sends HTML img object
-   byId('sendHTML').onclick = SendImageHTML;  
-   function SendImageHTML() {    
-        var imageHTML = convertCanvasToImage(canvas); 
-        log('sending image HTML: ');
-        log(imageHTML);
-        socket.emit('SendImageHTML', imageHTML);
+
+    // Un-used support functions around byte data
+    //////////////////////////////////////////////////////////////
+    
+   /**
+    * Called by SendImageToBytes. Creates an image based on convertCanvasToImage.
+    * Canvas -> Image -> Bytes
+    * @return {Uint8Array} Bytes from the newly converted canvas.
+    */
+    function ConvertImgToBytes() {
+        var image  = convertCanvasToImage(canvas);
+        var buffer = new ArrayBuffer(image.data.length);
+        var bytes  = new Uint8Array(buffer);
+
+        for (var i=0; i<bytes.length; i++) {
+            bytes[i] = image.data[i];
+        }
+        debug.log("ConvertImgToBytes:" );
+        Console.log(bytes);
+
+        return bytes;
+    }
 
 
-        // var htmlAsString = "'" + imageHTML + "'";
-        // log('HTML as string: ');
-        // log(htmlAsString); // RETURNS: '[object HTMLImageElement]'
-        // socket.emit('SendImageHTML', htmlAsString);
-   }
+    /**
+     * Accepts bytes from server & converts to an image.
+     * @return {ImageData} Image data from byte array
+     */
+    function ConvertBytesToImg (blob) {
+        var bytes     = new Uint8Array(blob);        
+        var imageData = context.createImageData(canvas.width, canvas.height);
+        
+        // Copy bytes to new imageData obj
+        for (var i=8; i<imageData.data.length; i++) {
+            imageData.data[i] = bytes[i];
+        }
+        // This can paint the canvas with the new-found image
+        // context.putImageData(imageData, 0, 0);
 
+        // OR
+
+        // Returns new image    
+        return imageData;
+    }  
 
 
   /**
@@ -264,12 +234,17 @@ var sampleImage = document.getElementById("ringoImage"),
         socket.emit('defImgBinary', bytes);
   }
 
+   /**
+    *  Convert canvas to image, and tell server to convert those bytes to an image
+    */
+   function SendImageToBytes() {    
+         var bytes = ConvertImgToBytes(image);
+        log('sending image binary. bytes: ');
+        log(bytes);
 
+        socket.emit('ConvertedImgToBytes', bytes);
+   }
 
-
-
-   //// Receive
-   /////////////////////////////////////////////////////
 
     // Receive bytes from server, convert them to an image, draw image on canvas
     socket.on("ConvertBytesToImg", function(bytes) {
@@ -283,37 +258,6 @@ var sampleImage = document.getElementById("ringoImage"),
     });
 
 
-    // draw line received from server
-	socket.on('draw_line', function (data) {
-        drawLines(data);
-   });
-
-
-    // Accept img buffer and draw it to canvas
-    socket.on("imageDiv", function(info) {
-        log('image data incomnig...');
-        log(info);
-        var img      = new Image();
-            img.src = 'data:image/jpeg;base64,' + info;
-        context.drawImage(img, 0, 0);
-    });
-
-   
-   // main loop, running every 25ms
-   function mainLoop() {
-      // check if the user is drawing
-      if (mouse.click && mouse.move && mouse.pos_prev) {
-         // send line to to the server
-         socket.emit('draw_line', {
-              line: [ mouse.pos, mouse.pos_prev ] ,
-              color: context.curColor // BUG: not working yet
-            });
-         mouse.move = false;
-      }
-      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-      setTimeout(mainLoop, 25);
-   }
-   mainLoop();
 
 
 });
